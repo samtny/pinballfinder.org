2a96d6290283721bb8115123657fb92cba2a2bff
Sam T <samtny@gmail.com>
2014-12-02 15:02:50 -0500

    init with 'git'

--- htdocs/protected/yiic.php
+++ htdocs/protected/yiic.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yiic='/home/sthompson/bin/yiic';
+$yiic='/home/sthompson/bin/yii-1.1.15.022a51/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
